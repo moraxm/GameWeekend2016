708b94c82ad7146ef12a406e058853edefa8fac5
victor martinez simon <victormartinezsimon@gmail.com>
2016-07-03 01:07:34 +0200

    improve cards

--- Assets/Scripts/CardScene.cs
+++ Assets/Scripts/CardScene.cs
@@ -8,31 +8,48 @@ public class CardScene : MonoBehaviour {
 
    public GameObject card;
 
+  public float timeAnimation = 1;
+
 	// Use this for initialization
 	void Start () {
     GameManager gm = GameManager.GetInstance();
     Card goBet = null;
+    GameObject visible;
 	  if(gm.player2points > gm.player1points)
     {
       m_win.SetActive(false);
       m_lose.SetActive(true);
+      visible = m_lose;
       //goBet = gm.Player1Bet;
     }
     else
     {
       m_win.SetActive(true);
       m_lose.SetActive(false);
+      visible = m_win;
      //goBet = gm.Player2Bet;
     }
 
     goBet.transform.position = card.transform.position;
 
+    StartCoroutine(PlayAnimation(visible));
+
 	}
 
-  public void ExitScreen ()
+  private IEnumerator PlayAnimation(GameObject final)
   {
+    Vector3 finalScale = final.transform.localScale;
+    final.transform.localScale = Vector3.zero;
+    float timeAcum = 0;
+    float timeWait = 0.05f;
+
+    while(timeAcum <= timeAnimation)
+    {
+      Vector3 new_scale = Vector3.Lerp(Vector3.zero, finalScale, timeAcum / timeAnimation);
+      final.transform.localScale = new_scale;
+      timeAcum += timeWait;
+      yield return new WaitForSeconds(timeWait);
+    }
 
   }
-
-
 }
