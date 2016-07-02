7edd758f53a1f42ef68237ba81df3a0087619d86
victor martinez simon <victormartinezsimon@gmail.com>
2016-07-03 00:42:20 +0200

    add lots of cards Modify the cart to have a border

---
+++ Assets/Scripts/VoteScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoteScreen : MonoBehaviour {
+
+  public CardUI m_card;
+
+	// Use this for initialization
+	void Start () {
+    m_card.fillUI(GameManager.GetInstance().GetRandomCard());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
