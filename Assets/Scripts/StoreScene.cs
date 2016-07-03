92eda5da688b326b3ba42abe93a0eddd0b711b5a
victor martinez simon <victormartinezsimon@gmail.com>
2016-07-03 12:49:36 +0200

    add store scene fix vote card scene

---
+++ Assets/Scripts/StoreScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreScene : MonoBehaviour {
+
+  public CardUI ui_card;
+	// Use this for initialization
+	void Start () {
+    Card c = GameManager.GetInstance().GetRandomCard();
+    ui_card.fillUI(c);
+	}
+	
+}
