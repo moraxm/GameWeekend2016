d27f245626fa1fe2829b4a59aba6e90065cd0fa0
Samuel <saguma@gmail.com>
2016-07-03 12:29:23 +0200

    SetQuantity in UI Type image with white color

--- Assets/Scripts/GetQuantity.cs
+++ Assets/Scripts/GetQuantity.cs
@@ -6,10 +6,10 @@ public class GetQuantity : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Card card = GetComponentInParent<Card>();
+        CardUI card = transform.parent.GetComponentInChildren<CardUI>();
         Text text = GetComponent<Text>();
         // TODO: 
-        int quantity = 1;// GameManager.GetInstance().;
+        int quantity = card.card.cardCount;
         text.text = "x" + quantity;
 	}
 	
