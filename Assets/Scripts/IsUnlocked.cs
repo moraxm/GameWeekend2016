534254458b3cb76b9bfc51dd28c9cb315331b9c0
victor martinez simon <victormartinezsimon@gmail.com>
2016-07-03 13:56:02 +0200

    fix collection scene

--- Assets/Scripts/IsUnlocked.cs
+++ Assets/Scripts/IsUnlocked.cs
@@ -6,9 +6,10 @@ using System.Collections.Generic;
 public class IsUnlocked : MonoBehaviour {
     
 
+
 	// Use this for initialization
 	void Start () {
-        Card card = gameObject.GetComponentInParent<Card>();
+        Card card = gameObject.transform.parent.GetComponentInChildren<CardUI>().card;
 
         List<Card> cards = GameManager.GetInstance().m_playerCardColletion[card.collection];
 
